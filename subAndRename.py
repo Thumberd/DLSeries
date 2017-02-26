29551ddf7bdbdd2154ae438a36d24320e6220f79
Albrecht Jérémy <ajeremyalbrecht@gmail.com>
2017-02-26 09:11:07 +0100

    Correction de bug lié au renommage de fichier avec les: ":"

--- subAndRename.py
+++ subAndRename.py
@@ -3,13 +3,12 @@ import re
 import os
 import sys
 
-key = "*"
+key = ""
 base_format_file = "{show} - {season}x{episode} - {name}.{extension}"
 
 
 def sub(idE, path):
     rSearch = requests.get("https://api.betaseries.com/subtitles/episode?key={}&id={}".format(key, idE))
-    print(rSearch.text)
     link = rSearch.json()['subtitles'][0]['url']
     rSub = requests.get(link)
     if rSub.status_code == 200:
@@ -19,11 +18,20 @@ def sub(idE, path):
                     f.write(chunk)
         else:
             return rSub.text
-    return  rSub.status_code
+    return rSub.status_code
+
+
+def adapt_path(path):
+    path = path.replace(':', '')
+    path = path.replace('?', '')
+    path = path.replace('*', '')
+    path = path.replace('<', '')
+    path = path.replace('>', '')
+    return path
 
 
 def get_episode(name, season, n_episode):
-    findID = requests.get("https://api.betaseries.com/shows/search?key={}&title={}".format(key, name))
+    findID = requests.get("https://api.betaseries.com/shows/search?key={}&title={}&order=followers".format(key, name))
     idS = int(findID.json()['shows'][0]['id'])
     rEpisodes = requests.get("https://api.betaseries.com/episodes/search?key={}&show_id={}&number=S{}E{}"
                              .format(key, idS, season, n_episode))
@@ -35,16 +43,18 @@ def get_show(name):
     name = name.replace('.', ' ')
     name = name.replace(':', ' ')
     name = name.replace(' ', '')
-    list = ["NCIS Los Angeles", 'NCIS', "The 100", "Quantico", "Blacklist", "Mr Robot",]
+    list = ["NCIS: Los Angeles", 'NCIS', "The 100", "Quantico", "Blacklist", "Mr Robot", "Westworld"]
     for show in list:
+        original = show
+        show = show.replace(':', ' ')
         if show.replace(' ', '') in name or show.replace(' ', '').lower() in name.lower():
-            return show
+            return original
     return None
 
 
 def rename_files(previous_video_file, future_video_file, base_path=os.getcwd()):
     try:
-        os.rename(base_path + '/' + previous_video_file, base_path + '/' + future_video_file)
+        os.rename(os.path.join(base_path, previous_video_file), os.path.join(base_path, adapt_path(future_video_file)))
     except Exception as e:
         return e
     else:
@@ -78,7 +88,7 @@ def fetch_episode(file_name, dest_path=None):
                                        extension=file_name.split('.')[-1])
         srt_name = base_format_file.format(show=show, season=season, episode=episode, name=episodeInfo['title'],
                                            extension='srt')
-        sub(episodeInfo['id'], os.getcwd() + '/' + srt_name)
+        sub(episodeInfo['id'], os.path.join(os.getcwd(), adapt_path(srt_name)))
         if dest_path != None:
             rename_files(file_name, name, dest_path)
         else:
